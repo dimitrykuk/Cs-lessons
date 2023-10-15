b64778a87cf7fe1bd0b88ef3f231c642c0ae5be9
dimitrykuk <dimitrykuklin1997@mail.ru>
2023-10-15 17:38:47 +0300

    any_users

---
+++ lesson5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string name1 = Console.ReadLine();
+if (name1.ToLower() == "настя")
+{
+    Console.WriteLine("Здравствуй, Насть!");
+}
+    else
+{
+    Console.Write("Здравствуйте, ");
+    Console.WriteLine(name1);  
+}
+
