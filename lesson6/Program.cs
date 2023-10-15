8332f8187e6e32095511635ffac51ed7823f3457
dimitrykuk <dimitrykuklin1997@mail.ru>
2023-10-15 17:48:55 +0300

    Girki

---
+++ lesson6/Program.cs
@@ -0,0 +1,13 @@
+﻿int gir1 = 3;
+int gir2 = 5;
+int gir3 = 8;
+int gir4 = 1;
+
+int max = gir1;
+
+if (max < gir1) max = gir1;
+if (max < gir2) max = gir2;
+if (max < gir3) max = gir3;
+if (max < gir3) max = gir3;
+
+Console.WriteLine(max);
